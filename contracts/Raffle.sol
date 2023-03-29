9ed229da5928bed4f35dbb9249338fb861e04d6a
tuagkcuf <jokersms222@gmail.com>
2023-03-29 18:32:54 +0300

    gitignore

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,14 +2,19 @@
 
 pragma solidity ^0.8.9;
 
-error Raffle__NotEnoughETHEntered
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract Raffle {
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle is VRFConsumerBaseV2 {
     // State variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    // Events
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
     
@@ -18,6 +23,15 @@ contract Raffle {
             revert Raffle__NotEnoughETHEntered();
         }
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function pickRandomWinner() external {
+
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        
     }
 
     function getEntranceFee() public view returns(uint256) {
