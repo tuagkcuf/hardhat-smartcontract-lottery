4b1bae2135d9a737338a87f60308c7c6897c4268
tuagkcuf <jokersms222@gmail.com>
2023-03-29 16:35:43 +0300

    add functions to store

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -13,13 +13,18 @@ contract Raffle {
         i_entranceFee = entranceFee;
     }
     
-    function enterRaffle() {
+    function enterRaffle() public payable {
         if(msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
+        s_players.push(payable(msg.sender));
     }
 
     function getEntranceFee() public view returns(uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
