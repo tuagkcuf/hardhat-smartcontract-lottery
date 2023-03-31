cb589fbd7c9f2577f7b85fbe4e9c04c07faf33b7
tuagkcuf <jokersms222@gmail.com>
2023-03-31 13:47:19 +0300

    deployment for contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,4 +8,12 @@ require("dotenv").config()
 
 module.exports = {
     solidity: "0.8.18",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
