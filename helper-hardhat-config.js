4b9b0549c14b269406ce0b540439ed8793ded409
tuagkcuf <jokersms222@gmail.com>
2023-03-31 14:42:18 +0300

    make

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,6 +2,17 @@ const networkConfig = {
     11155111: {
         name: "sepolia",
         vrfCoordinatorV2: "0xab18414CD93297B0d12ac29E63Ca20f515b3DB46",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        callbackGasLimit: "500000",
     }
 }
 
